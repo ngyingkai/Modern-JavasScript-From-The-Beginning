9e76d34b0bf2725842f02174882d9f7984b5057d
Ng Ying Kai <yingkai96@outlook.com>
2019-10-16 22:31:18 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Modern-JavasScript-From-The-Beginning" 
---
+++ app.js
@@ -0,0 +1,27 @@
+// Single Line Comment
+
+/*
+  Multi
+  Line
+  Comment
+*/
+
+// Logging string to console
+console.log('Hello World'); // Strings
+console.log(123); // Integers
+console.log(true); // Boolean
+var greeting = 'Hello World!'; // Variable
+console.log(greeting); // Variable
+console.log([1,2,3,4]); // Arrays
+console.log({a:1, b:2}); // Objects
+console.table({a:1, b:2}); // Print out objects as table
+console.error('This is an error'); // Error
+console.clear(); // Clear console
+console.warn("This is a warning"); // Warning
+console.time("Hello"); // Start timer with Hello as identifier
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd("Hello"); // Stop and output time taken to run commands
